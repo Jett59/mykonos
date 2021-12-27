3658d582946ee1fa57129434420d3488ce1f02c9
Jett Thompson <jett.thompson.59@gmail.com>
2021-12-27 15:10:16 +1100

    made the memcpy and memset implementations faster by using the cpu rep movs/stos instructions with 8-byte copies

---
+++ kernel/arch/x86_64/kernel/memory/copying.cpp
@@ -0,0 +1,43 @@
+/*
+    Copyright (C) 2021  Jett Thompson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    */
+#include <string.h>
+
+extern "C" void *memset(void *str, int c, size_t n) {
+  void *tmpStr = str;
+  __asm__ volatile("rep stosq"
+                   : "=D"(tmpStr)
+                   : "a"((unsigned char)c * 0x0101010101010101), "c"(n / 8)
+                   : "memory");
+  __asm__ volatile("rep stosb"
+                   :
+                   : "D"(tmpStr), "c"(n & 7), "a"((unsigned char)c)
+                   : "memory");
+  return str;
+}
+extern "C" void *memcpy(void *dst, const void *src, size_t n) {
+  void *tmpDst = dst;
+  const void *tmpSrc = src;
+  __asm__ volatile("rep movsq"
+                   : "=D"(tmpDst), "=S"(tmpSrc)
+                   : "c"(n / 8)
+                   : "memory");
+  __asm__ volatile("rep movsb"
+                   :
+                   : "D"(tmpDst), "S"(tmpSrc), "c"(n & 7)
+                   : "memory");
+  return dst;
+}
