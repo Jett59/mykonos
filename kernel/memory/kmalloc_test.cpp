80d3b7defec6cb09788448afb07d84448d0a1fb9
Jett Thompson <jett.thompson.59@gmail.com>
2021-12-29 10:09:55 +1100

    re-implemented the kmalloc function to use a fixed size header to record the size and also added a test to prove that the kmalloc function can allocate infinitely

---
+++ kernel/memory/kmalloc_test.cpp
@@ -0,0 +1,54 @@
+/*
+    Copyright (C) 2021  Jett Thompson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    */
+#ifdef RUNTIME_TESTS
+
+#include <kmalloc.h>
+#include <test.h>
+
+namespace memory {
+namespace test {
+bool kmallocTest(::test::Logger logger) {
+  logger("kmallocTest:\n");
+  // Stress test: can it allocate forever?
+  void *allocatedMemory[2048];
+  bool passed = true;
+  int numAllocated = 0;
+  for (int i = 0; i < 2048; i++) {
+    allocatedMemory[i] = kmalloc(2);
+    numAllocated++;
+    if (allocatedMemory[i] == nullptr) {
+      passed = false;
+      break;
+    }
+  }
+  // Yes, clean up
+  for (int i = 0; i < numAllocated; i++) {
+    kfree(allocatedMemory[i]);
+  }
+  if (!passed) {
+    logger("kmallocTest: Failed stress test\n");
+    return false;
+  }
+  logger("kmallocTest: Passed stress test\n");
+  logger("kmallocTest: Succeeded\n");
+  return true;
+}
+ADD_TEST(kmallocTest);
+} // namespace test
+} // namespace memory
+
+#endif
