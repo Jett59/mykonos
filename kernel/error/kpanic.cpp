639b322b587b5ffb754ff305fbcb9e289fa402c1
Jett Thompson <jett.thompson.59@gmail.com>
2021-12-30 16:31:16 +1100

    fixed some of the formatting of kpanic.cpp

--- kernel/error/kpanic.cpp
+++ kernel/error/kpanic.cpp
@@ -14,11 +14,11 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#include <kpanic.h>
-#include <kout.h>
 #include <cpu.h>
+#include <kout.h>
+#include <kpanic.h>
 
-extern "C" [[noreturn]] void kpanic(const char* message) {
+extern "C" [[noreturn]] void kpanic(const char *message) {
   kout::print("Kpanic!\n");
   kout::print(message);
   cpu::hault();
