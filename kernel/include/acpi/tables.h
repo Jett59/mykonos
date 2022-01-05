c8e7c7cb4757b4ed0d636dc7d72e78412a13b9f2
Jett Thompson <jett.thompson.59@gmail.com>
2022-01-05 15:15:17 +1100

    created some useful structures for the table header and the table managers

---
+++ kernel/include/acpi/tables.h
@@ -0,0 +1,46 @@
+/*
+    Copyright (C) 2022  Jett Thompson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    */
+#ifndef _ACPI_TABLES_H
+#define _ACPI_TABLES_H
+
+#include <stdint.h>
+
+namespace acpi {
+struct TableHeader {
+  char signature[4];
+  uint32_t length;
+  uint8_t revision;
+  uint8_t checksum;
+  char oemId[6];
+  char oemTableId[8];
+  uint32_t oemRevision;
+  char creatorId[4];
+  uint32_t creatorRevision;
+};
+static_assert(sizeof(TableHeader) == 36, "TableHeader incorrect size");
+
+enum class TableType {};
+class TableManager {
+public:
+  const TableType type;
+
+protected:
+  TableManager(TableType type) : type(type) {}
+};
+} // namespace acpi
+
+#endif
