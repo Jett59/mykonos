00d2ccc0371fc29928a906f43819a49b3f857ae7
Jett Thompson <jett.thompson.59@gmail.com>
2022-04-17 10:30:19 +1000

    added the header file for the cleaner

---
+++ kernel/include/mykonos/cleaner.h
@@ -0,0 +1,28 @@
+/*
+    Copyright (C) 2022 Jett Thompson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef _MYKONOS_CLEANER_H
+#define _MYKONOS_CLEANER_H
+
+#include <mykonos/kmalloc.h>
+
+namespace cleaner {
+void addObject(void *object, void (*callback)(void *) = memory::kfree);
+
+void init();
+} // namespace cleaner
+
+#endif
