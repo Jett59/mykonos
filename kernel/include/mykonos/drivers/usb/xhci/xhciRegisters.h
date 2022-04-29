c313e9a812d51d71252a5c131b6d63207aaa4c70
Jett Thompson <jett.thompson.59@gmail.com>
2022-04-29 19:27:53 +1000

    created some header files for xhci stuff

---
+++ kernel/include/mykonos/drivers/usb/xhci/xhciRegisters.h
@@ -0,0 +1,47 @@
+/*
+    Copyright (C) 2022 Jett Thompson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef _MYKONOS_DRIVERS_USB_XHCI_XHCI_REGISTERS_H
+#define _MYKONOS_DRIVERS_USB_XHCI_XHCI_REGISTERS_H
+
+#include <mykonos/mmio.h>
+
+#include <stdint.h>
+
+namespace drivers {
+namespace xhci {
+struct XhciRegisters {
+  uint8_t capabilityLength;
+  uint8_t reserved0;
+  uint16_t version;
+  // ...
+};
+class XhciRegisterAccess {
+public:
+  XhciRegisterAccess(XhciRegisters *registers) : registers(registers) {}
+  
+  uint8_t getCapabilityLength() {
+    return mmio::read(&registers->capabilityLength);
+  }
+  uint16_t getVersion() { return mmio::read(&registers->version); }
+
+private:
+  XhciRegisters *registers;
+};
+} // namespace xhci
+} // namespace drivers
+
+#endif
