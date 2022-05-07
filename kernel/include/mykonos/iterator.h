0bd1df04dffca64910cda35be91f53e604dabe28
Jett Thompson <jett.thompson.59@gmail.com>
2022-05-07 19:47:27 +1000

    implemente the RangedIterator

---
+++ kernel/include/mykonos/iterator.h
@@ -0,0 +1,81 @@
+/*
+    Copyright (C) 2022 Jett Thompson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef _MYKONOS_ITERATOR_H
+#define _MYKONOS_ITERATOR_H
+
+#include <stddef.h>
+
+namespace util {
+template <typename T> class RangedIterator {
+public:
+  RangedIterator(T *begin, T *end, T *pointer)
+      : begin(begin), end(end), pointer(pointer) {}
+
+  T &operator[](size_t index) const {
+    return pointer + index < end && pointer + index >= begin ? pointer[index]
+                                                             : {};
+  }
+  T &operator*() { return (*this)[0]; }
+
+  RangedIterator<T> operator++(int) {
+    RangedIterator<T> result = *this;
+    pointer++;
+    return result;
+  }
+  RangedIterator<T> &operator++() {
+    pointer++;
+    return *this;
+  }
+  RangedIterator<T> operator+(size_t offset) const {
+    return {begin, end, pointer + offset};
+  }
+  RangedIterator<T> operator--(int) {
+    RangedIterator<T> result = *this;
+    pointer--;
+    return result;
+  }
+  RangedIterator<T> &operator--() {
+    pointer--;
+    return *this;
+  }
+  RangedIterator<T> operator-(size_t offset) const {
+    return {begin, end, pointer - offset};
+  }
+
+  size_t operator-(const RangedIterator<T> &other) const {
+    if (other.begin == begin && other.end == end) {
+      return pointer - other.pointer;
+    } else {
+      return -1;
+    }
+  }
+
+  bool operator==(const RangedIterator<T> &other) const {
+    return other.begin == begin && other.end == end && other.pointer == pointer;
+  }
+  bool operator!=(const RangedIterator<T> &other) const {
+    return !((*this) == other);
+  }
+
+private:
+  T *begin;
+  T *end;
+  T *pointer;
+};
+} // namespace util
+
+#endif
